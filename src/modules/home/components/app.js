5618963b1a2f8c24c9661cfe7f5a8337303d119b
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-10-31 22:02:34 -0500

    Clean up

--- src/modules/home/components/app.js
+++ src/modules/home/components/app.js
@@ -1,31 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { requestAuthorization } from "../api/auth";
-import requestTrack from "../api/requestTrack";
 import search from "../api/search";
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const [authToken, setAuthToken] = useState();
 
   useEffect(() => {
     requestAuthorization().then((res) => {
-      setLoading(false);
       setAuthToken(res.access_token);
     });
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
+  if (!authToken) {
+    return <div>Connecting to Spotify API...</div>;
   }
   return (
     <div className="w-full flex flex-col h-full gap-7 px-4 py-6 justify-center items-center">
       <h3>New project</h3>
-      <button
-        className="bg-red-800 text-white rounded p-3"
-        onClick={() => requestTrack(authToken)}
-      >
-        Request track
-      </button>
       <button
         className="bg-blue-800 text-white rounded p-3"
         onClick={() => search(authToken)}
