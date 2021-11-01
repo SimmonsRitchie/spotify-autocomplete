9c50fd113fc67c59943ed780154bc3417c9d971f
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-11-01 12:15:31 -0500

    Begin building out track listing

--- src/modules/home/components/modal/body-album.js
+++ src/modules/home/components/modal/body-album.js
@@ -1,8 +1,21 @@
 import React from "react";
+import dayjs from "dayjs";
 
-const BodyAlbum = ({dataType, data}) => {
-  
-  return <div className="">modal</div>;
+
+const BodyAlbum = ({data}) => {
+  console.log('album data',data)
+  const releaseDate = data.release_date
+  const fmtReleaseDate = dayjs(releaseDate).format('MMMM DD, YYYY')
+  return <div className="flex flex-col gap-4">
+  <p>Released: {fmtReleaseDate}</p>
+  <div>
+  <p className="mb-1">Tracks:</p>
+  <ul>
+  <li>Track 1</li>
+  <li>Track 2</li>
+  </ul>
+  </div>
+  </div>;
 };
 
 export default BodyAlbum;
