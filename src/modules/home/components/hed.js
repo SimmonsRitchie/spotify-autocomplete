cadc58433517794fd7abf3a3225ac140f6ca3dfa
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-11-02 07:44:40 -0500

    Tweak styles

--- src/modules/home/components/hed.js
+++ src/modules/home/components/hed.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Hed = () => {
   return (
-    <h1 className="font-bold font-maven text-gray-60 text-5xl sm:text-6xl text-center text-gray-100">
-      <span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-300">
+    <h1 className="font-bold font-maven text-gray-60 text-5xl sm:text-6xl text-center">
+      <span className="text-transparent bg-clip-text bg-gradient-to-b from-purple-50 to-gray-300">
         Tune Finder
       </span>
     </h1>
