496c8324a7bf418feeb96c4fe83e08fc93b42f51
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-11-01 21:51:12 -0500

    clean up and clarify error msgs

--- src/modules/home/api/auth.js
+++ src/modules/home/api/auth.js
@@ -1,21 +1,12 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Client Credentials oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
- */
-
 const request = require("request-promise");
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID; // Your client id
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET; // Your secret
 
 if (typeof CLIENT_ID === "undefined") {
-  throw new Error("CLIENT_ID is undefined. Make sure it's set in .env");
+  throw new Error("REACT_APP_CLIENT_ID is undefined. Make sure it's set in .env");
 }
 if (typeof CLIENT_SECRET === "undefined") {
-  throw new Error("CLIENT_SECRET is undefined. Make sure it's set in .env");
+  throw new Error("REACT_APP_CLIENT_SECRET is undefined. Make sure it's set in .env");
 }
 
 // your application requests authorization
