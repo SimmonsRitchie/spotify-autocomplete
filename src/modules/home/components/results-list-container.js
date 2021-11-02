8ea0c3b591226c471a01dc4ce21e34ccc0e3e24d
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-11-01 21:22:55 -0500

    set column alignment

--- src/modules/home/components/results-list-container.js
+++ src/modules/home/components/results-list-container.js
@@ -3,7 +3,7 @@ import { DATATYPE_STYLES_LOOKUP } from "../api/contants";
 
 const ResultsListContainer = ({ children, dataType }) => {
   return (
-    <div id={dataType} className="w-full bg-blue-100 rounded px-6 py-5">
+    <div id={dataType} className="w-full bg-blue-100 rounded px-6 py-5 flex flex-col">
       <h6 className="inline-block font-maven text-gray-900 rounded font-semibold mb-6 text-3xl sm:text-4xl">
         {DATATYPE_STYLES_LOOKUP[dataType].labelPlural}
       </h6>
