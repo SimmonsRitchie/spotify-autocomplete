d3ce30c26fec6e7f32c80bf8d6bc22d694f0f9c5
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-11-02 07:38:37 -0500

    Improve styling

--- src/modules/home/components/error-msg.js
+++ src/modules/home/components/error-msg.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const ErrorMsg = () => {
   return (
-    <div className="max-w-sm w-full bg-red-800 shadow py-6 px-4 rounded text-white  text-center">
+    <div className="max-w-sm w-full shadow-sm bg-red-200 text-sm py-8 px-4 rounded text-red-900  text-center">
       <p className="pb-4">Whoops! Sorry, something went wrong.</p>
-
-      <p>Try again later</p>
+      <p>Try again later.</p>
     </div>
   );
 };
